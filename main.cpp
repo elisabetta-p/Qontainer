25764ffe4914e7028ab376453a2979c286f1435d
elisabetta-p <elisabetta.piombin@gmail.com>
2018-11-12 19:15:47 +0100

    boh ??? non romperti ti prego 2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
 #include <QGuiApplication>
-<<<<<<< HEAD
-=======
-#include <QQmlApplicationEngine>
->>>>>>> fe6294d336eaaf5cc1efbc4d90fb170585e45495
 #include <QWidget>
 
 int main(int argc, char *argv[])
@@ -10,6 +6,7 @@ int main(int argc, char *argv[])
    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
    QGuiApplication app(argc, argv):
+
 
    return app.exec();
 }

e0955dc7361f1d51f378867f1e5e29305801a0ae
elisabetta-p <elisabetta.piombin@gmail.com>
2018-12-26 18:23:40 +0100

    ??????????

--- ProgettoP2.pro
+++ ProgettoP2.pro
@@ -2,7 +2,6 @@ QT += core
 CONFIG += c++11
 CONFIG+=sdk_no_version_check
 QT += widgets
-QT += qml
 
 # The following define makes your compiler emit warnings if you use
 # any feature of Qt which as been marked deprecated (the exact warnings
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
 #include <QWidget>
 
 int main(int argc, char *argv[])
@@ -8,10 +7,6 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
 
     return app.exec();
 }

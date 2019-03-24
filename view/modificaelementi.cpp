20c3c2f761c3b99e614c73c717753b393487464f
Elisabetta <elisabetta.piombin@gmail.com>
2019-03-24 11:11:57 +0100

    secondo tentativo di caricamento file di modifica

---
+++ view/modificaelementi.cpp
@@ -0,0 +1,6 @@
+#include "modificaelementi.h"
+
+modificaelementi::modificaelementi()
+{
+
+}
---
+++ view/modificaelementi.h
@@ -0,0 +1,11 @@
+#ifndef MODIFICAELEMENTI_H
+#define MODIFICAELEMENTI_H
+
+
+class modificaelementi
+{
+public:
+    modificaelementi();
+};
+
+#endif // MODIFICAELEMENTI_H
